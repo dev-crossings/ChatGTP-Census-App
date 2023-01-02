3aac2b704635a6e6c79ed6b463bba99e3ffdf513
dev-crossings <113070251+dev-crossings@users.noreply.github.com>
2023-01-02 08:43:05 -0800

    Add Chinese, remove "county" "California"

--- Census-Asian-Streamlit.py
+++ Census-Asian-Streamlit.py
@@ -6,7 +6,7 @@ import seaborn as sns
 @st.cache
 def load_data():
     # Make a request to the US Census API to get the data
-    response = requests.get("https://api.census.gov/data/2020/acs/acs5?get=NAME,B01001_001E,B06009_002E,B06009_003E&for=county:*&in=state:06")
+    response = requests.get("https://api.census.gov/data/2020/acs/acs5?get=NAME,B01001_001E,B06009_002E,B06009_003E,B06009_004E&for=county:*&in=state:06")
 
 
     # Convert the response to JSON
@@ -16,11 +16,16 @@ def load_data():
     df = pd.DataFrame(data[1:], columns=data[0])
 
     # Clean the data
-    df = df[["NAME", "B01001_001E", "B06009_002E", "B06009_003E"]]
-    df.rename(columns={"NAME": "County", "B01001_001E": "Total population", "B06009_002E": "Japanese", "B06009_003E": "Filipino"}, inplace=True)
+    df = df[["NAME", "B01001_001E", "B06009_002E", "B06009_003E", "B06009_004E"]]
+    df.rename(columns={"NAME": "County", "B01001_001E": "Total population", "B06009_002E": "Japanese", "B06009_003E": "Filipino", "B06009_004E": "Chinese"}, inplace=True)
+    df["County"] = df["County"].str.replace(" County,", "")  # Remove " County" from the county names
+    df["County"] = df["County"].str.replace(" California", "")  # Remove " California" from the county names
+    
+    df["Chinese"] = df["Chinese"].astype(int)
     df["Japanese"] = df["Japanese"].astype(int)
     df["Filipino"] = df["Filipino"].astype(int)
     df["Total population"] = df["Total population"].astype(int)
+    df["Chinese_pct"] = df["Chinese"] / df["Total population"]
     df["Japanese_pct"] = df["Japanese"] / df["Total population"]
     df["Filipino_pct"] = df["Filipino"] / df["Total population"]
 
@@ -35,17 +40,17 @@ def main():
     # Sort the counties alphabetically
     df = df.sort_values("County")
 
-    st.title("Percentage of CA county for either Japanese or Filipino populations")
+    st.title("Percentage of CA county for Japanese or Filipino or Chinese populations")
     st.header("data derived from 2020 US Census ACS data")
 
     # Add a multiselect to select one or more counties
-    county_names = st.multiselect("Select one or more counties:", df["County"].tolist(), ["Sacramento County, California", "San Francisco County, California"])
+    county_names = st.multiselect("Select one or more counties:", df["County"].tolist(), ["Sacramento", "San Francisco"])
 
     # Filter the data for the selected counties
     county_df = df[df["County"].isin(county_names)]
 
     # Add a dropdown menu to select Japanese or Filipino data
-    population_type = st.selectbox("Select population type:", ["Japanese", "Filipino"])
+    population_type = st.selectbox("Select population type:", ["Japanese", "Filipino", "Chinese"])
 
     if population_type == "Japanese":
         # Create a plot using Seaborn
@@ -54,11 +59,20 @@ def main():
         plot.set_title("Japanese Population in California Counties")
         plot.set_xlabel("County")
         plot.set_ylabel("Percentage of total population")
-    else:
+    
+    elif population_type == "Filipino":
         # Create a plot using Seaborn
         sns.set_style("darkgrid")
         plot = sns.barplot(x="County", y="Filipino_pct", data=county_df)
         plot.set_title("Filipino Population in California Counties")
+        plot.set_xlabel("County")
+        plot.set_ylabel("Percentage of total population")
+
+    else:
+        # Create a plot using Seaborn
+        sns.set_style("darkgrid")
+        plot = sns.barplot(x="County", y="Chinese_pct", data=county_df)
+        plot.set_title("Chinese Population in California Counties")
         plot.set_xlabel("County")
         plot.set_ylabel("Percentage of total population")
 
